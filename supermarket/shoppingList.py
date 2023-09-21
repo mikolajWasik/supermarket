a53e91ca6febebf2260dd010781cd5dba3000ad8
Mikołaj <wm46681@zut.edu.pl>
2023-09-22 01:24:46 +0200

    Fixing errors and preventing user from making them

--- supermarket/shoppingList.py
+++ supermarket/shoppingList.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QDialog, QStackedWidget, QHeaderView, QMessageBox, QInputDialog, QLineEdit, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QDialog, QStackedWidget, QHeaderView, QMessageBox, QInputDialog, QLineEdit, QTableWidgetItem, QWidget, QCheckBox, QHBoxLayout
 from PyQt5.uic import loadUi
 from PyQt5 import QtCore, QtWidgets
 
@@ -13,6 +13,8 @@ class ShoppingList(QDialog):
         loadUi('interfaces/shoppingList.ui', self)
         self.widget = widget
         self.productsForSavingList = []
+        self.stateList = []
+        self.checkBoxList = []
 
         header = self.shoppingListTable.horizontalHeader()
         header.setSectionResizeMode(0, QHeaderView.ResizeToContents)
@@ -38,20 +40,29 @@ class ShoppingList(QDialog):
 
 
     def loadProducts(self):
+        self.shoppingListTable.clearContents()
+        self.shoppingListTable.setRowCount(0)
         self.shoppingListTable.setRowCount(len(self.productsForSavingList))
         for i in range(len(self.productsForSavingList)):
             self.shoppingListTable.setItem(i, 0, QTableWidgetItem(str(self.productsForSavingList[i][0])))
             self.shoppingListTable.setItem(i, 1, QTableWidgetItem(str(self.productsForSavingList[i][1])))
+            self.shoppingListTable.setCellWidget(i, 2, self.checkBoxList[i])
+            self.shoppingListTable.setItem(i, 2, QTableWidgetItem(''))
 
 
     def addToList(self):
         product = self.allProductsComboBox.currentText()
+        if product == "Choose from list...":
+            return
         quantity, okPressed = QInputDialog.getText(self, "Supermarket", "Enter quantity: ", QLineEdit.Normal, "")
         if okPressed == False or quantity == '':
             return
         try:
+            quantity = quantity.replace(',', '.')
             quantity = float(quantity)
-            if quantity % 1 == 0:
+            if quantity <= 0:
+                raise ValueError("Quantity must be greater than zero")
+            elif quantity % 1 == 0:
                 quantity = int(quantity)
         except Exception as e:
             msg = QMessageBox()
@@ -60,7 +71,16 @@ class ShoppingList(QDialog):
             msg.exec_()
             return
 
-        self.productsForSavingList.append((product, quantity, False))
+        Widget = QWidget()
+        CheckBox = QCheckBox()
+        self.stateList.append(CheckBox)
+        Layout = QHBoxLayout(Widget)
+        Layout.addWidget(CheckBox)
+        Layout.setAlignment(QtCore.Qt.AlignCenter)
+        Layout.setContentsMargins(0, 0, 0, 0)
+        Widget.setLayout(Layout)
+        self.checkBoxList.append(Widget)
+        self.productsForSavingList.append([product, quantity])
         self.loadProducts()
 
 
